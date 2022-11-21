bfa7bce944210f74050c5c1851026acf6ef19f0c
Gisela Rios <gisee_rios@hotmail.com>
2022-11-21 12:01:52 -0300

    navbar

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,16 @@
+import './navBar.css'
+
+
+function Navbar () {
+    return(
+        <nav>
+            <h1>logo</h1>
+            <button className="botones-nav">Boton 1</button>
+            <button className="botones-nav">Boton 2</button>
+            <button className="botones-nav">Boton 3</button>
+            <button className="botones-nav">Boton 4</button>
+        </nav>
+    )
+}
+
+export default Navbar;
